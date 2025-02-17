bef6deee1ba95defba8da6a7db0c57a000db58be
Patricio Duran <patricio@ac95.ca>
2025-02-17 16:15:34 -0500

    MonthSoftware - Ok

--- Resources/MonthSoftware.Designer.cs
+++ Resources/MonthSoftware.Designer.cs
@@ -59,5 +59,194 @@ namespace MultiLanguages.Resources {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Backup &amp; Recovery.
+        /// </summary>
+        public static string BackupRecovery {
+            get {
+                return ResourceManager.GetString("BackupRecovery", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Feature Updates &amp; Enhancements.
+        /// </summary>
+        public static string FeatureUpdates {
+            get {
+                return ResourceManager.GetString("FeatureUpdates", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Maintenance.
+        /// </summary>
+        public static string MaintenanceSub {
+            get {
+                return ResourceManager.GetString("MaintenanceSub", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Maintenance.
+        /// </summary>
+        public static string MaintenanceTitle {
+            get {
+                return ResourceManager.GetString("MaintenanceTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Monthly Software Maintenance.
+        /// </summary>
+        public static string MonthlySoftwareMaintenance {
+            get {
+                return ResourceManager.GetString("MonthlySoftwareMaintenance", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Monthly.
+        /// </summary>
+        public static string MonthlySub {
+            get {
+                return ResourceManager.GetString("MonthlySub", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Monthly.
+        /// </summary>
+        public static string MonthlyTitle {
+            get {
+                return ResourceManager.GetString("MonthlyTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Our.
+        /// </summary>
+        public static string OurTitle {
+            get {
+                return ResourceManager.GetString("OurTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Performance Optimization.
+        /// </summary>
+        public static string PerformanceOptimization {
+            get {
+                return ResourceManager.GetString("PerformanceOptimization", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Security Updates &amp; Monitoring.
+        /// </summary>
+        public static string SecurityUpdates {
+            get {
+                return ResourceManager.GetString("SecurityUpdates", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Software Maintenance.
+        /// </summary>
+        public static string SoftwareMaintenance {
+            get {
+                return ResourceManager.GetString("SoftwareMaintenance", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Software.
+        /// </summary>
+        public static string SoftwareSub {
+            get {
+                return ResourceManager.GetString("SoftwareSub", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Software.
+        /// </summary>
+        public static string SoftwareTitle {
+            get {
+                return ResourceManager.GetString("SoftwareTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ensure your software runs smoothly with our comprehensive monthly SaaS maintenance and optimization services. We manage everything from performance tuning to security updates, keeping your software up-to-date and your users satisfied..
+        /// </summary>
+        public static string Text1 {
+            get {
+                return ResourceManager.GetString("Text1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We provide ongoing maintenance to ensure your software runs at optimal performance. From updates and patches to bug fixes, we make sure your SaaS solution remains reliable and fully functional for your users..
+        /// </summary>
+        public static string Text2 {
+            get {
+                return ResourceManager.GetString("Text2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We optimize your software for better performance, ensuring fast load times, smooth functionality, and minimal downtime. Our performance enhancements help reduce user frustration and increase overall satisfaction..
+        /// </summary>
+        public static string Text3 {
+            get {
+                return ResourceManager.GetString("Text3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We ensure your software remains secure with regular security updates and ongoing monitoring. Our team addresses potential vulnerabilities, keeping your data and user information safe..
+        /// </summary>
+        public static string Text4 {
+            get {
+                return ResourceManager.GetString("Text4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Our support team is available to assist your users with any issues or inquiries. We quickly resolve user-reported issues, ensuring minimal downtime and a positive user experience..
+        /// </summary>
+        public static string Text5 {
+            get {
+                return ResourceManager.GetString("Text5", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We provide regular updates to add new features and improve existing functionalities based on user feedback and market trends. This helps keep your software competitive and aligned with user needs..
+        /// </summary>
+        public static string Text6 {
+            get {
+                return ResourceManager.GetString("Text6", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We ensure that your software’s data is backed up regularly and provide recovery solutions in case of unexpected issues. This service guarantees that your software and data remain safe, with minimal disruption to your users..
+        /// </summary>
+        public static string Text7 {
+            get {
+                return ResourceManager.GetString("Text7", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to User Support &amp; Issue Resolution.
+        /// </summary>
+        public static string UserSupport {
+            get {
+                return ResourceManager.GetString("UserSupport", resourceCulture);
+            }
+        }
     }
 }
