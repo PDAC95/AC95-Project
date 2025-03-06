e0745efb920f0244e9b1a9a1c4c9b820852d0e93
Patricio Duran <patricio@ac95.ca>
2025-03-05 20:50:05 -0500

    OK - Contact

--- Resources/Shared.Designer.cs
+++ Resources/Shared.Designer.cs
@@ -106,6 +106,15 @@ namespace MultiLanguages.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Contact.
+        /// </summary>
+        public static string Contact {
+            get {
+                return ResourceManager.GetString("Contact", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Date:.
         /// </summary>
         public static string Date {
@@ -124,11 +133,29 @@ namespace MultiLanguages.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Get in touch!.
+        /// </summary>
+        public static string Getintouch {
+            get {
+                return ResourceManager.GetString("Getintouch", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Get Started.
         /// </summary>
         public static string GetStarted {
             get {
                 return ResourceManager.GetString("GetStarted", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Go Home.
+        /// </summary>
+        public static string GoHome {
+            get {
+                return ResourceManager.GetString("GoHome", resourceCulture);
             }
         }
         
@@ -210,6 +237,15 @@ namespace MultiLanguages.Resources {
         public static string Subscribe {
             get {
                 return ResourceManager.GetString("Subscribe", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Thank you for reaching out!.
+        /// </summary>
+        public static string Thank {
+            get {
+                return ResourceManager.GetString("Thank", resourceCulture);
             }
         }
         
